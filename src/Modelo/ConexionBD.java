ec1a2fef7b4cf45d33c88d346b0edb8c7458ed6e
Juanse <Juanse@Efestus>
2023-11-08 00:04:24 -0500

    Se modifico autor, se añadieron clases

---
+++ src/Modelo/ConexionBD.java
@@ -0,0 +1,63 @@
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Juan Ochoa
+ */
+public class ConexionBD {
+
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+    private static final String USERNAME = "system";
+    private static final String PASSWORD = "admin";
+
+    private Connection connection;
+
+    /**
+     * Obtiene la conexión actual.
+     *
+     * @return la conexión actual.
+     */
+    public Connection getConnection() {
+        return connection;
+    }
+
+    /**
+     * Abre la conexión a la base de datos.
+     *
+     * @throws SQLException si ocurre un error al abrir la conexión.
+     * @throws ClassNotFoundException si no se encuentra el controlador de la
+     * base de datos.
+     */
+    public void openConnection() throws SQLException, ClassNotFoundException {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver"); // Cargar el controlador de la base de datos Oracle
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD); // Establecer la conexión con la base de datos
+            System.out.println("Conexión establecida");
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Error al abrir la conexión: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    /**
+     * Cierra la conexión a la base de datos.
+     *
+     * @throws SQLException si ocurre un error al cerrar la conexión.
+     */
+    public void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            try {
+                connection.close(); // Cerrar la conexión con la base de datos
+                System.out.println("Conexión cerrada exitosamente");
+            } catch (SQLException e) {
+                System.err.println("Error al cerrar la conexión: " + e.getMessage());
+                throw e;
+            }
+        }
+    }
+}
